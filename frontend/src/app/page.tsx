115a874af89e384ef27636c186f6b4e06c72f340
Deepak Vats <deepakvats123123@gmail.com>
2024-08-02 02:01:55 +0530

    some styles bugs fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,20 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
 import Image from "next/image";
+import grp1 from "../../public/grp1.jpg"
+import grp2 from "../../public/grp2.jpg"
 
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-     
-    </main>
+    <div>
+      <div className="text-center m-5 text-blue-400 font-bold">
+        <h1 className="sm:text-4xl text-xl">SHOP BY CATEGORY</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-5 content-evenly m-5">
+        <div className="border-4"><Image className="w-full" src={grp1} alt="male Grp"   /></div>
+        <div className="border-4"><Image className="w-full h-full" src={grp2} alt="female Grp"   /></div>
+      </div>
+
+    </div>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,16 +3,20 @@ import { ThemeToggle } from './ThemeToggle'
 import Link from 'next/link'
 import { FaUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
+import Image from 'next/image';
+import slog from '../../public/s-log.png'
+import { Input } from "@/components/ui/input"
 
 
 
 const Navbar = () => {
   return (
-    <div className='grid sm:grid-cols-3 grid-cols-2 gap-5 border-2 p-3 bg-'>
+    <div className='grid sm:grid-cols-3 grid-cols-2 gap-5 border-2 p-3 sticky top-0 dark:bg-slate-950 bg-white'>
 
         <div className='text-left sm:col-start-1'> 
         <Link href={'/'}>
-            <h1 className='font-bold text-2xl'>Super-Mart</h1>
+            {/* <h1 className='font-bold text-2xl'>Super-Mart</h1> */}
+            <Image src={slog} alt='S-Logo' width={40} height={40} />
         </Link>
         </div>
 
@@ -20,12 +24,13 @@ const Navbar = () => {
             <Link className='font-bold hover:text-blue-400' href={'/'}>Home</Link>
             <Link className='font-bold hover:text-blue-400' href={'/men'}>Men</Link>
             <Link className='font-bold hover:text-blue-400' href={'/women'}>Women</Link>
+            <Input type='text' placeholder='Search for products, brands and more' />
         </div>
 
         <div className='flex items-center justify-end sm:col-start-3 col-start-2'>
             <ThemeToggle />
             <Link className='font-bold ml-5 hover:text-blue-400 flex items-center' href={'/login'}> <FaUser className='mr-2'/> Login</Link>
-            <Link className='font-bold ml-5 hover:text-blue-400 flex items-center' href={'/cart'}><FaShoppingCart className='mr-2' /> Cart</Link>
+            <Link className='font-bold ml-5 hover:text-blue-400 flex items-center' href={'/cart'}><FaShoppingCart /></Link>
         </div>
     </div>
   )
