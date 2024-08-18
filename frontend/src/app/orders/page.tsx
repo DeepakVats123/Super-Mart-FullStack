5a6a137c21678783edfab107dd536c010884537f
Deepak Vats <deepakvats123123@gmail.com>
2024-08-19 02:08:53 +0530

    fully responsiveness done

--- frontend/src/app/orders/page.tsx
+++ frontend/src/app/orders/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
@@ -25,34 +24,36 @@ const Orders = () => {
   },[lsOrders])
   return (
     <>
-      <div className='m-5 sm:px-20'>
-        <h1 className='font-bold text-xl'>My Orders</h1>
-      </div>
+      <div className='m-auto sm:w-[500px] w-[96%]'>
+        <div className='m-5'>
+          <h1 className='font-bold text-xl'>My Orders</h1>
+        </div>
 
-      <div className='m-5 sm:px-20'>
-          { Array.isArray(orders) && orders.length > 0? orders.slice().reverse().map((order:any,ind)=>{
-              return <div key={ind+1+"a"} className='flex border p-2 mb-2 items-center gap-4 rounded-lg'>
-                        <div className=''>
-                            <div className='w-12'>
-                              <Image className='w-full' src={order.items[0].image_url} alt="" />
-                            </div>    
-                            <div className='border mt-1 w-12'>
-                                <p className='text-xs'>{order.items.length} more</p>
-                            </div>
-                        </div>
-                        
-                        <div>
-                          <span className='font-bold text-gray-600 text-lg hidden md:inline'>Order Total-</span>  <span className='font-bold text-lg'>Rs. {order.order_total}</span>
-                        </div>
-                        
-                        <div>
-                          <span className='text-gray-600 text-xs'>Date-{order.date} Time-{order.time}</span>
-                        </div>
+        <div className=''>
+            { Array.isArray(orders) && orders.length > 0? orders.slice().reverse().map((order:any,ind)=>{
+                return <div key={ind+1+"a"} className='flex border p-2 mb-2 items-center gap-5 rounded-lg justify-evenly sm:max-w-[500px]' >
+                          <div className=''>
+                              <div className='w-16'>
+                                <img className='w-full' src={order.items[0].image_url} alt="" />
+                              </div>    
+                              <div className='border mt-1 w-16 text-center'>
+                                  <p className='text-xs'>{order.items.length} {order.items.length > 1 ? "items" : "item"}</p>
+                              </div>
+                          </div>
+                          
+                          <div>
+                            <span className='font-bold text-gray-600 text-lg hidden md:inline'>Order Total</span> <br /> <span className='font-bold text-lg'>Rs. {order.order_total}</span>
+                          </div>
+                          
+                          <div>
+                            <span className='text-gray-600 text-xs'>Date-{order.date}</span> <br /> <span className='text-gray-600 text-xs'>{order.time}</span>
+                          </div>
 
-                     </div>
-          })
-          : <div>{"You don't have any order history"}</div>
-        }
+                      </div>
+            })
+            : <div>{"You don't have any order history"}</div>
+          }
+        </div>
       </div>
     </>
   )
